56f2da89e182cb5fb19689552e2438126bd62287
Michael Kuczynski <mkuczyns@ucalgary.ca>
2019-01-14 16:19:05 -0700

    Updated Python script

--- src/automatic_BMD_calculation.py
+++ src/automatic_BMD_calculation.py
@@ -31,35 +31,41 @@
 #   -Add error checking (try/catch)
 #   -Add functionality for multiple DICOM series in a single directory
 #   -Crop phantom ROIs
-#   -Enhance contrast
-#   -Convert to mask
 #   -Optional dilate/erode
 #   -Hough Circle Transform
 #   -Crop individual phantoms
 #   -Calculate BMD and save data
 #----------------------------------------------------- 
 
+<<<<<<< HEAD
+from sys import argv
+from os import listdir, path
+=======
 import sys
 import os
+>>>>>>> b40b5d9ee73d728f13f5aeb2ca43fb0e00bc2414
 import itk
 
 # Check input arguements
-if len(sys.argv) < 2:
+if len(argv) < 2:
     print("ERROR: Incorrect script usage:")
-    print("Usage Option #1: python " + sys.argv[0] + " <Dicom_Directory>")
-    print("Usage Option #2: python " + sys.argv[0] + " <Dicom_Directory> <Output_NIfTI_FILENAME>")
-    sys.exit(1)
+    print("Usage Option #1: python " + argv[0] + " <Dicom_Directory>")
+    print("Usage Option #2: python " + argv[0] + " <Dicom_Directory> <Output_NIfTI_FILENAME>")
+    exit(1)
 
 # Use the specified directory
-dirName = sys.argv[1]
+dirName = argv[1]
+
+# Get the number of DICOMs in the specified diretory
+count = len([name for name in listdir(dirName) if path.isfile(path.join(dirName, name))])
 
 count = len([name for name in os.listdir(dirName) if os.path.isfile(os.path.join(dirName, name))])
 
 # Create input and output image variables
 # Using NIfTI temporarily...
 output_filename = "temp.nii"
-if len(sys.argv) > 2:
-    output_filename = sys.argv[2]
+if len(argv) > 2:
+    output_filename = argv[2]
 
 # Specify the DICOM image parameters
 PixelType = itk.ctype("signed short")
@@ -82,7 +88,7 @@ seriesUID = namesGenerator.GetSeriesUIDs()
 # Check if directory contains DICOMs
 if len(seriesUID) < 1:
     print("No DICOMs in: " + dirName)
-    sys.exit(1)
+    exit(1)
 
 # Print out all DICOM series in the provided directory
 print("The directory: " + dirName + " contains the following DICOM Series: ")
@@ -108,10 +114,13 @@ for uid in seriesUID:
     reader.Update()
 
     # 01 - Crop Phantom ROIs
+<<<<<<< HEAD
     #---------------------------------------------------------------------------------------
     # ROI #1 = (265, 410) -> (400, 460)
     # ROI #2 = (...) -> (...)
 
+=======
+>>>>>>> b40b5d9ee73d728f13f5aeb2ca43fb0e00bc2414
     size_x = 135
     size_y = 50
     size_z = count - 1
@@ -121,7 +130,11 @@ for uid in seriesUID:
 
     print("Cropping ROI #1...")
 
+<<<<<<< HEAD
+    cropper = itk.ExtractImageFilter.New(reader.GetOutput())
+=======
     cropper = itk.ExtractImageFilter.New(Input = reader.GetOutput())
+>>>>>>> b40b5d9ee73d728f13f5aeb2ca43fb0e00bc2414
     extraction_region = cropper.GetExtractionRegion()
     size = extraction_region.GetSize()
     size[0] = int(size_x)
@@ -137,15 +150,19 @@ for uid in seriesUID:
     cropper.Update()
 
     # 02 - Filter
+<<<<<<< HEAD
     #---------------------------------------------------------------------------------------
     # Median filter, radius = 3
 
+=======
+>>>>>>> b40b5d9ee73d728f13f5aeb2ca43fb0e00bc2414
     MF_radius = 3
     print("Applying median filter with radius = " + str(MF_radius))
     median = itk.MedianImageFilter.New(Input = cropper.GetOutput(), Radius = MF_radius)
     median.Update()
 
     # 03 - Adaptive Histogram Equalization
+<<<<<<< HEAD
     #---------------------------------------------------------------------------------------
     # Alpha = 1 -> unsharp mask
     # Beta  = 0
@@ -171,19 +188,26 @@ for uid in seriesUID:
     #---------------------------------------------------------------------------------------
     # Otsu Segmentation
 
+    num_bins = 128
+    num_thresh = 1
+    label_offset = 0
+
+    print("Applying Otsu's method for image segmentation with " + str(num_bins) + " bins, "
+            + str(num_thresh) + " threshold(s), and a label offset of " + str(label_offset))
+
     thresholdFilter = itk.OtsuMultipleThresholdsImageFilter[ImageType, ImageType].New()
     thresholdFilter.SetInput(histogramEqualization.GetOutput())
 
     # Default values
-    thresholdFilter.SetNumberOfHistogramBins(128)
-    thresholdFilter.SetNumberOfThresholds(1)
-    thresholdFilter.SetLabelOffset(0)
+    thresholdFilter.SetNumberOfHistogramBins(num_bins)
+    thresholdFilter.SetNumberOfThresholds(num_thresh)
+    thresholdFilter.SetLabelOffset(label_offset)
     thresholdFilter.Update()
 
     rescaler = itk.RescaleIntensityImageFilter[ImageType, ImageType].New()
     rescaler.SetInput(thresholdFilter.GetOutput())
     rescaler.SetOutputMinimum(0)
-    rescaler.SetOutputMaximum(255)
+    rescaler.SetOutputMaximum(125)
     rescaler.Update()
 
     # 05 - Optional Erode/Dilate
@@ -193,7 +217,12 @@ for uid in seriesUID:
 
     # 06 - Hough Circle Transform
     #---------------------------------------------------------------------------------------
-    circle = itk.HoughTransform2DCirclesImageFilter.New()
+    
+    print("Starting Hough Circle Transform...")
+
+    AccumulatorImageType = itk.Image[itk.F, Dimension]
+
+    circle = itk.HoughTransform2DCirclesImageFilter[].New()
     circle.SetInput(rescaler.GetOutput())
     circle.SetMinimumRadius(30.0)
     circle.SetMaximumRadius(40.0)
@@ -201,18 +230,51 @@ for uid in seriesUID:
 
     # 07 - Create individual ROIs
     #---------------------------------------------------------------------------------------
+    # Use data from the Hough Circle Transform
 
 
     # 08 - Calculate BMD
     #---------------------------------------------------------------------------------------
 
+=======
+    AHE_alpha = float(1)
+    AHE_beta = float(0)
+    AHE_radius = int(5)
+
+    print("Appying adaptive histogram equalization with alpha = " + str(AHE_alpha) + 
+            ", beta = " + str(AHE_beta) + ", and radius = " + str(AHE_radius))
+
+    histogramEqualization = itk.AdaptiveHistogramEqualizationImageFilter.New(median.GetOutput())
+    histogramEqualization.SetAlpha(AHE_alpha)
+    histogramEqualization.SetBeta(AHE_beta)
+    histogramEqualization.Update()
+
+    radius = itk.Size[Dimension]()
+    radius.Fill(AHE_radius)
+    histogramEqualization.SetRadius(radius)
+
+    # 04 - Threshold
+
+    # 05 - Optional Erode/Dilate
+
+    # 06 - Hough Circle Transform
+
+    # 07 - Create individual ROIs
+
+    # 08 - Calculate BMD
+
+>>>>>>> b40b5d9ee73d728f13f5aeb2ca43fb0e00bc2414
     print("\nWriting out " + output_filename + "...")
 
     # Write out the modified images as NIfTI
     writer = itk.ImageFileWriter[ImageType].New()
     writer.SetFileName(output_filename)
     writer.UseCompressionOn()
+<<<<<<< HEAD
     writer.SetInput(circle.GetOutput())
+=======
+    writer.SetInput(histogramEqualization.GetOutput())
+>>>>>>> b40b5d9ee73d728f13f5aeb2ca43fb0e00bc2414
     writer.Update()
 
     # To avoid an infinite loop...
